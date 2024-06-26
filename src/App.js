8969b209aad82dbc742a940dc107835efad30a4d
Prem B. Shah <techie.prembshah@gmail.com>
2024-06-26 10:31:08 +0530

    Added a Cart Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Products from './components/Products/Products';
+import CartComponent from './components/cartComponent/CartComponent';
 function App() {
   return (
       <div className="App">
+        <CartComponent />
         <Products  />
       </div>
   );
---
+++ src/components/cartComponent/CartComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function CartComponent() {
+    const items = useSelector((state) => {
+        return state.items;
+    });
+   
+    
+  return (
+    <div className='cart'>
+        {Object.values(items).map((item) => {
+            return (
+                <div key={item.id}>
+                    <div>
+                        <h2>
+                            {item.title}
+                        </h2>
+                    <span>Quantity : {item.quantity}</span>
+                    </div>
+                    <span>Price : {item.price}</span>
+                </div>
+            );
+        })}
+    </div>
+  )
+}
+
+export default CartComponent
